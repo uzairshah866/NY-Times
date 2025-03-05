dc0fbe1e67ffcde91a03fdbf71ce7b3ea94c00f8
uzairshah866 <uzairshahh121@gmail.com>
2025-03-05 22:52:27 +0500

    env file'

--- src/services/api.js
+++ src/services/api.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import axios from 'axios'
 
-const API_KEY = "EhEAuvfmivAoBzjdDpMs2u51qTod3YvM";
-const BASE_URL = "https://api.nytimes.com/svc/mostpopular/v2/viewed";
+const API_KEY = import.meta.env.VITE_NYT_API_KEY
+const BASE_URL = 'https://api.nytimes.com/svc/mostpopular/v2/viewed'
 
 export const fetchArticles = async (period = 1) => {
   try {
     const response = await axios.get(
       `${BASE_URL}/${period}.json?api-key=${API_KEY}`
-    );
-    return response.data.results;
+    )
+    return response.data.results
   } catch (error) {
-    console.error("Error fetching articles:", error);
-    return [];
+    console.error('Error fetching articles:', error)
+    return []
   }
-};
+}
